dc465bb839bc52d6ba05a2643b387c99f4bccd45
surethingbro <leobretonb9@gmail.com>
2025-10-15 13:35:34 -0500

    15/10/25
    Adding code to test the motors :D

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTesting.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class motorTesting extends LinearOpMode {
+
+    public DcMotor motor;
+    public DcMotor motor2;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        motor = hardwareMap.get(DcMotor.class, "motor");
+        motor2 = hardwareMap.get(DcMotor.class, "motor2");
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+
+
+            motor.setPower(1);
+            motor2.setPower(1);
+
+        }
+    }
+}
